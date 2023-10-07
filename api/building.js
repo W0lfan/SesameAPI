30bc0fc0794a5208583298f4bdc974cbf07c6bad
naflouille <naflouille@proton.me>
2023-10-07 09:50:01 +0200

    Update building.js

--- api/building.js
+++ api/building.js
@@ -57,7 +57,7 @@
                     gathering.some(gather =>
                         item.name.toLowerCase().includes(gather) ||
                         (item.description && item.description.toLowerCase().includes(gather.toLowerCase())) ||
-                        (item.author && Array.isArray(item.author) && item.author.some(aut => aut && Array.isArray(aut.name) && aut.name.some(name => name.toLowerCase().includes(gather.toLowerCase()))))                    )
+                        (item.author && Array.isArray(item.author) && item.author.some(aut => aut && aut.name.toLowerCase().includes(gather.toLowerCase()))))
                 );
     
     
